74280374593449a3151c7b98cfbe7d4a95dd703d
Nick Skliar-Davies <nick.skliar-davies@dcslsoftware.com>
2024-06-13 13:10:40 +0100

    sys: fewer render loops
    fix: remove empty index.js

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 'use client'
 
+import React from "react";
 import Image from "next/image";
 import { useState } from 'react';
 import styles from "./page.module.css";
@@ -35,54 +36,70 @@ let scriptHasRun = false;
 let error = false;
 let mdError = false;
 
+let lastFetched = '';
 export default function PillRunner() {
   const [code, setCode] = useState<string>('');
   const [markdown, setMarkdown] = useState<string>('');
   const [result, setResult] = useState<string[]>([]);    
   const [scriptIndex, setScriptIndex] = useState<number>(0);
+  const [error, setError] = useState(false);
+  const [mdError, setMdError] = useState(false);
   
-  fetch(`${scriptLocations[scriptIndex]}index.js`)
-  .then(
-    (result) => {
-      if (result.ok){
-        result.text().then(x => {                    
-          error = false;
-          setCode(x);
-          runScript(x);
-        });
-      } else {
-        error = true;
-      }        
-    })
-  .catch((error) => console.error(error));
-
-  fetch(`${scriptLocations[scriptIndex]}README.md`)
-  .then(
-    (result) => {
-      if (result.ok){
-        result.text().then(x => {                    
-          mdError = false;
-          const mdList = x.split('---');
-          mdList.shift();
-          mdList.shift();
-          const mdText = mdList.join('---');
-
-          setMarkdown(mdText);          
-        });
-      } else {
-        mdError = true;
-      }        
-    })
-  .catch((error) => console.error(error));
-
+  if (scriptLocations[scriptIndex] != lastFetched){
+    fetch(`${scriptLocations[scriptIndex]}index.js`)
+    .then(
+      (result) => {
+        if (result.ok){
+          result.text().then(x => {                    
+            setError(false);
+            console.info('setCode triggering render loop');
+            setCode(x);
+            runScript(x);
+          });
+        } else {
+          console.error(result.statusText);
+          setError(true);
+        }        
+      })
+    .catch((error) => {
+      console.error(error);
+      setError(true);
+    });
+  
+    fetch(`${scriptLocations[scriptIndex]}README.md`)
+    .then(
+      (result) => {
+        if (result.ok){
+          result.text().then(x => {                    
+            setMdError(false);
+            const mdList = x.split('---');
+            mdList.shift();
+            mdList.shift();
+            const mdText = mdList.join('---');
+            console.info('setMarkdown triggering render loop');
+            setMarkdown(mdText);          
+          });
+        } else {
+          setMdError(true);
+        }        
+      })
+    .catch((error) => {
+      console.error(error);
+      setMdError(true);
+    });
+    lastFetched = `${scriptLocations[scriptIndex]}`;
+  }
+  
 
   function runScript(scriptCode: string){    
     externalArray.length = 0;
     if (!scriptHasRun){
-      scriptHasRun = true;
-      setResult([]);   
-      eval(scriptCode)           
+      scriptHasRun = true;         
+      eval(scriptCode)     
+      console.info('setResult triggering render loop');      
+      setResult([...externalArray]);
     }    
+    
   }  
 
   console.log = (...args) => {
@@ -91,8 +108,7 @@ export default function PillRunner() {
       logStatement += `${arg},`;
     });    
     
-    externalArray.push(logStatement);
-    setResult([...externalArray]);        
+    externalArray.push(logStatement);         
     console.info(...args);
   };  
 
@@ -107,6 +123,7 @@ export default function PillRunner() {
           }
           console.info(`${index} index of scripts`);
           scriptHasRun = false;
+          console.info('setScriptIndex triggering render loop');
           setScriptIndex(index);               
           }}>Next Pill</button>  
       <div className={styles.description}>
@@ -147,17 +164,20 @@ export default function PillRunner() {
               An error has occurred retrieving the pill script. This may not have been provided.
             </span>        
           : 
+          <React.Fragment>
             <SyntaxHighlighter language="javascript">
-              {code}
-            </SyntaxHighlighter>                          
+                {code}
+            </SyntaxHighlighter>  
+            <div>
+              {result.map((val: string, index: number) => (
+                <p className={styles.codeline} key={`val-${index}`}>{val}</p>
+              ))}                        
+            </div>      
+          </React.Fragment>                    
           }    
         
             
-        <div>
-          {result.map((val: string, index: number) => (
-            <p className={styles.codeline} key={`val-${index}`}>{val}</p>
-          ))}                        
-        </div>   
+         
         </div>
       </div>
                     
