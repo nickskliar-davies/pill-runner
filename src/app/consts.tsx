de3af471c65a6caefb12ec9f4a846cd5a32ea7c7
Nick Skliar-Davies <nick.skliar-davies@dcslsoftware.com>
2024-07-03 16:57:41 +0100

    feature: menu to view pills

--- src/app/consts.tsx
+++ src/app/consts.tsx
@@ -21,3 +21,157 @@ export const scriptLocations = ["./pills/Array.prototype.every/"
     ,"./pills/shuffle-array-elements/"
     ,"./pills/using-!!operator/"  
   ];
+
+
+export const scriptLocationExtended = [
+  {
+    name: 'Array of Consecutive Numbers',
+    url: './pills/array-of-consecutive-numbers/',
+    scriptSource: '',
+    mdSource: 'README.md'
+  },
+  {
+    name: 'Array Truncation',
+    url: './pills/array-truncation/',
+    scriptSource: '',
+    mdSource: 'README.md'
+  },
+  {
+    name: 'Array.prototype.every',
+    url: './pills/Array.prototype.every/',
+    scriptSource: 'index.js',
+    mdSource: 'README.md'
+  },
+  {
+    name: 'Assert Testing',
+    url: './pills/assert-testing/',
+    scriptSource: '',
+    mdSource: 'README.md'
+  },
+  {
+    name: 'Async Await Iterations',
+    url: './pills/async-await-iterations/',
+    scriptSource: 'index.js',
+    mdSource: 'README.md'
+  },
+  {
+    name: 'Check Conditions',
+    url: './pills/check-conditions/',
+    scriptSource: 'index.js',
+    mdSource: 'README.md'
+  },
+  {
+    name: 'Check variables with And Operator',
+    url: './pills/check-variables-with-and-operator/',
+    scriptSource: '',
+    mdSource: 'README.md'
+  },
+  {
+    name: 'Clone Objects',
+    url: './pills/clone-objects/',
+    scriptSource: '',
+    mdSource: 'README.md'
+  },
+  {
+    name: 'Compose',
+    url: './pills/compose/',
+    scriptSource: 'index.js',
+    mdSource: 'README.md'
+  },
+  {
+    name: 'Console Explained',
+    url: './pills/console-explained/',
+    scriptSource: 'index.js',
+    mdSource: 'README.md'
+  },
+  {
+    name: 'Currying',
+    url: './pills/currying/',
+    scriptSource: 'index.js',
+    mdSource: 'README.md'
+  },
+  {
+    name: 'Dedupe arrays',
+    url: './pills/dedupe-arrays/',
+    scriptSource: 'index.js',
+    mdSource: 'README.md'
+  },
+  {
+    name: 'Default Values',
+    url: './pills/DefaultValues/',
+    scriptSource: '',
+    mdSource: 'README.md'
+  },
+  {
+    name: 'Falsy Values',
+    url: './pills/falsy-values/',
+    scriptSource: 'index.js',
+    mdSource: 'README.md'
+  },
+  {
+    name: 'In Operator',
+    url: './pills/in-operator/',
+    scriptSource: '',
+    mdSource: 'README.md'
+  },
+  {
+    name: 'Map',
+    url: './pills/map/',
+    scriptSource: '',
+    mdSource: 'README.md'
+  },
+  {
+    name: 'Merging Arrays',
+    url: './pills/merging-arrays/',
+    scriptSource: 'index.js',
+    mdSource: 'README.md'
+  },
+  {
+    name: 'Prevent Prototype Pollution',
+    url: './pills/prevent-prototype-pollution/',
+    scriptSource: '',
+    mdSource: 'README.md'
+  },
+  {
+    name: 'Reduce',
+    url: './pills/reduce/',
+    scriptSource: '',
+    mdSource: 'README.md'
+  },
+  {
+    name: 'Regular Expressions',
+    url: './pills/regular-expressions/',
+    scriptSource: '',
+    mdSource: 'README.md'
+  },
+  {
+    name: 'Replace All',
+    url: './pills/replaceAll/',
+    scriptSource: '',
+    mdSource: 'README.md'
+  },
+  {
+    name: 'Semicolon Usage',
+    url: './pills/semicolon-usage/',
+    scriptSource: '',
+    mdSource: 'README.md'
+  },
+  {
+    name: 'Short-circuit conditionals',
+    url: './pills/short-circuit-conditionals/',
+    scriptSource: '',
+    mdSource: 'README.md'
+  },
+  {
+    name: 'Shuffle Array Elements',
+    url: './pills/shuffle-array-elements/',
+    scriptSource: '',
+    mdSource: 'README.md'
+  },
+  {
+    name: 'Using !! Operator',
+    url: './pills/using-!!operator/',
+    scriptSource: '',
+    mdSource: 'README.md'
+  },
+];  
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,11 +7,11 @@ import styles from "./page.module.css";
 import Markdown from "react-markdown";
 import remarkGfm from "remark-gfm";
 import SyntaxHighlighter from 'react-syntax-highlighter';
-import { scriptLocations } from "./consts";
+import {  scriptLocationExtended } from "./consts";
 
 const externalArray: string[] = [];
 let scriptHasRun = false;
-
+let timerStart = false;
 let lastFetched = '';
 export default function PillRunner() {
   const [code, setCode] = useState<string>('');
@@ -20,87 +20,74 @@ export default function PillRunner() {
   const [scriptIndex, setScriptIndex] = useState<number>(0);
   const [error, setError] = useState(false);
   const [mdError, setMdError] = useState(false);
-  
-  if (scriptLocations[scriptIndex] != lastFetched){
-    fetch(`${scriptLocations[scriptIndex]}index.js`)
-    .then(
-      (result) => {
-        if (result.ok){
-          result.text().then(x => {                    
-            setError(false);
-            console.info('setCode triggering render loop');
-            setCode(x);
-            runScript(x);
-          });
-        } else {
-          console.error(result.statusText);
-          setError(true);
-        }        
-      })
-    .catch((error) => {
-      console.error(error);
-      setError(true);
-    });
-  
-    fetch(`${scriptLocations[scriptIndex]}README.md`)
-    .then(
-      (result) => {
-        if (result.ok){
-          result.text().then(x => {                    
-            setMdError(false);
-            const mdList = x.split('---');
-            mdList.shift();
-            mdList.shift();
-            const mdText = mdList.join('---');
-            console.info('setMarkdown triggering render loop');
-            setMarkdown(mdText);          
-          });
-        } else {
-          setMdError(true);
-        }        
-      })
-    .catch((error) => {
-      console.error(error);
-      setMdError(true);
-    });
-    lastFetched = `${scriptLocations[scriptIndex]}`;
+  const [activeScript, setActiveScript] = useState(scriptLocationExtended[0]);    
+
+  if (activeScript.scriptSource && lastFetched != activeScript.url){
+    scriptHasRun = false;
+    fetch(`${activeScript.url}${activeScript.scriptSource}`)
+      .then((result) => {
+        result.text().then(x => {
+          lastFetched = activeScript.url;
+          setCode(x);
+          runScript(x);
+          setError(false);
+        });
+      }).catch((error) => {
+        console.error(error);
+        setError(true);
+      });
+  }
+
+  if (activeScript.mdSource && activeScript.url != lastFetched){
+    fetch(`${activeScript.url}${activeScript.mdSource}`)
+      .then((result => {
+        result.text().then(x => {
+          lastFetched = activeScript.url;
+          const mdList = x.split('---');
+          mdList.shift();
+          mdList.shift();
+          const mdText = mdList.join('---');
+          setMarkdown(mdText);
+          setMdError(false);
+        });
+      })).catch((error) => {
+        console.error(error);
+        setError(true);
+      });
   }  
 
-  function runScript(scriptCode: string){    
-    externalArray.length = 0;
+  function runScript(scriptCode: string){        
     if (!scriptHasRun){
+      externalArray.length = 0;
       scriptHasRun = true;         
       eval(scriptCode)     
       console.info('setResult triggering render loop');      
+      console.info(externalArray);
       setResult([...externalArray]);
-    }    
-    
+    }        
   }  
 
   console.log = (...args) => {
-    let logStatement = ''
+    let logStatement = '';
+    console.info(...args);
     args.forEach(arg => {
-      logStatement += `${arg},`;
+      
+      logStatement += JSON.stringify(arg);
     });    
     
-    externalArray.push(logStatement);         
-    console.info(...args);
+    externalArray.push(logStatement);             
   };  
 
+  if (!timerStart){
+    timerStart = true;
+    setInterval(() => {
+      console.info(...externalArray);
+      setResult([...externalArray]);
+    }, 1000);
+  }
+  
   return (
-    <main className={styles.main}>
-      <button onClick={(e) => {     
-          let index = scriptIndex;        
-          if (scriptIndex < scriptLocations.length - 1){
-            index ++;
-          } else {
-            index = 0;
-          }
-          console.info(`${index} index of scripts`);
-          scriptHasRun = false;
-          console.info('setScriptIndex triggering render loop');
-          setScriptIndex(index);               
-          }}>Next Pill</button>  
+    <main className={styles.main}>      
       <div className={styles.description}>
         <div>
           <a
@@ -118,10 +105,21 @@ export default function PillRunner() {
               priority
             />
           </a>
+          <h1>Pill Runner</h1>
         </div>
       </div>
       <div className={styles.center}>
-        <div className={styles.markdown}>
+        <div className={styles.navbar}>
+          <ul>
+            {scriptLocationExtended.map((info, index) => (
+              <li key={`loc-${index}`}>
+                <button onClick={(e) => setActiveScript(info)}>{info.name}</button>
+              </li>
+            ))}
+          </ul>
+          
+        </div>
+        <div className={styles.markdown}>          
         {mdError ?
           <div>
             An error has occurred retrieving the markdown. This may not have been provided.
@@ -133,32 +131,28 @@ export default function PillRunner() {
         } 
         </div>
         <div>
-              
-          {error ?         
-            <span>
-              An error has occurred retrieving the pill script. This may not have been provided.
-            </span>        
-          : 
-          <React.Fragment>
-            <SyntaxHighlighter language="javascript">
-                {code}
-            </SyntaxHighlighter>  
-            <div>
-              {result.map((val: string, index: number) => (
-                <p className={styles.codeline} key={`val-${index}`}>{val}</p>
-              ))}                        
-            </div>      
-          </React.Fragment>                    
-          }    
-        
-            
-         
+          {!!activeScript.scriptSource && 
+            <React.Fragment>
+              {error ?         
+                <span>
+                  An error has occurred retrieving the pill script. This may not have been provided.
+                </span>        
+              : 
+                <React.Fragment>
+                  <SyntaxHighlighter language="javascript">
+                    {code}
+                  </SyntaxHighlighter>  
+                  <div>
+                    {result.map((val: string, index: number) => (
+                      <p className={styles.codeline} key={`val-${index}`}>{val}</p>
+                    ))}                        
+                  </div>      
+                </React.Fragment>                    
+              }    
+            </React.Fragment>
+          }                                       
         </div>
       </div>
-                    
-      
-
-      
     </main>
   );
 }
